982823c032ef2b4fedfc71d41bf678093b4b4cb6
Isaak Sheridan <newdevcoding@gmail.com>
2023-09-24 18:48:46 -0700

    added functionality to display in correct format

--- script.js
+++ script.js
@@ -59,7 +59,21 @@ compute() {
 }
 
 getDisplayNumber(number){
-    return number
+    const stringNumber = number.toString()
+    const integerDigits = parseFloat(stringNumber.split('.')[0])
+    const decimalDigits = stringNumber.split('.')[1]
+    let integerDisplay
+    if (isNaN(integerDigits)){
+        integerDisplay = ''
+    } else {
+        integerDisplay = integerDigits.toLocaleString('en', {
+        maximumFractionDigits: 0 })
+    }
+    if (decimalDigits != null){
+        return `${integerDisplay}.${decimalDigits}`
+    } else {
+        return integerDisplay
+    } 
 }
 
 updateDisplay() {
@@ -68,6 +82,9 @@ updateDisplay() {
     if (this.operation != null){
         this.previousOperandTextElement.innerText =
          `${this.previousOperand} ${this.operation}`
+
+    } else {
+        this.previousOperandTextElement.innerText = ''
     }
 
 }
